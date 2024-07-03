6f1637ad1f0aa35e94ee3f6301c82b613da50180
chow5566 <767656961@qq.com>
2024-07-03 19:09:09 +0800

    init

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,15 +5,13 @@
 <template>
   <div>
     <header class="h-[50px] border-b border-gray-200 flex items-center justify-between px-4">
-      <RouterLink to="/">
-        <a href="#" class="flex items-center">
-          <img :src="logoPath" class="h-[30px]">
+        <RouterLink to="/" class="flex items-center">
+          <img :src="logoPath" class="h-[30px]" alt="logo" />
           <div class="ml-[10px]">
             <div class="text-bold">智心内部测试工具</div>
             <div class="text-[12px] text-gray-500">仅供内部测试使用，请勿外传</div>
           </div>
-        </a>
-      </RouterLink>
+        </RouterLink>
     </header>
     <slot></slot>
   </div>
